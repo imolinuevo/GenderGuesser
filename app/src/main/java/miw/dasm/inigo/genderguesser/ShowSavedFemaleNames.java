66d6ee630f3f06b6b7837035591b2c45dfd71be9
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-30 17:45:43 +0100

    Show Female names layout done. Iñigo Molinuevo

---
+++ app/src/main/java/miw/dasm/inigo/genderguesser/ShowSavedFemaleNames.java
@@ -0,0 +1,13 @@
+package miw.dasm.inigo.genderguesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowSavedFemaleNames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_female_names);
+    }
+}
