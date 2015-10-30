42fd7999b0a2166a8ad1b8fa8626f198d3b8719c
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-30 17:42:37 +0100

    Show Male names layout done. Iñigo Molinuevo

---
+++ app/src/main/java/miw/dasm/inigo/genderguesser/ShowSavedMaleNames.java
@@ -0,0 +1,13 @@
+package miw.dasm.inigo.genderguesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowSavedMaleNames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_male_names);
+    }
+}
