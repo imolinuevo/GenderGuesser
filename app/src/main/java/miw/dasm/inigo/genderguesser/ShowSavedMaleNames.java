9aab58c8a140908f55fbd19ff3c45ea84a1f5dc2
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-31 11:48:08 +0100

    All persistency working. Iñigo Molinuevo

--- app/src/main/java/miw/dasm/inigo/genderguesser/ShowSavedMaleNames.java
+++ app/src/main/java/miw/dasm/inigo/genderguesser/ShowSavedMaleNames.java
@@ -2,6 +2,9 @@ package miw.dasm.inigo.genderguesser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowSavedMaleNames extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class ShowSavedMaleNames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_saved_male_names);
+        FirstNameStore firstNameStore = new FirstNameStore(this);
+        ArrayList<FirstName> firstNames = firstNameStore.getMaleFirstNames();
+        GenderFirstNameAdapter recentFirstNameAdapter = new GenderFirstNameAdapter(this, android.R.layout.simple_spinner_dropdown_item, firstNames);
+        ListView recentList = (ListView) findViewById(R.id.male_name_list);
+        recentList.setAdapter(recentFirstNameAdapter);
     }
 }
